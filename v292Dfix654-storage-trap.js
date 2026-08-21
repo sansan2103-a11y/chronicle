99ada340beedb8bbd20de903c2d5e8efdcb7d738
sansan2103-a11y <sansan2103@gmail.com>
2026-08-22 05:12:58 +0900

    fix728: BUILD marker only (no functional change)

--- v292Dfix654-storage-trap.js
+++ v292Dfix654-storage-trap.js
@@ -56,7 +56,7 @@
   } catch(e){ return; }
 
   var TAG   = '[v292Dfix654]';
-  var BUILD = '20260821-fix727';
+  var BUILD = '20260821-fix728';
   var METHODS = ['getItem', 'setItem', 'removeItem'];
   var EXPANDO = '__v346raw';
   var MAXLOG  = 40;
