ed53273bb0455d23b23a1546c691430ea7b73ae9
sansan2103-a11y <sansan2103@gmail.com>
2026-08-21 13:37:55 +0900

    fix719 release 4/6: fix654 BUILD tag -> 20260821-fix719

--- v292Dfix654-storage-trap.js
+++ v292Dfix654-storage-trap.js
@@ -56,7 +56,7 @@
   } catch(e){ return; }
 
   var TAG   = '[v292Dfix654]';
-  var BUILD = '20260821-fix718';
+  var BUILD = '20260821-fix719';
   var METHODS = ['getItem', 'setItem', 'removeItem'];
   var EXPANDO = '__v346raw';
   var MAXLOG  = 40;
